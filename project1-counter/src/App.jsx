7f2281816524f2e8d62ff308b7986556f50e8042
omin <pym000526@gmail.com>
2025-12-26 16:37:28 +0900

    Upload project1

---
+++ project1-counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Simple Counter</h1>
+      <div className="counter-display">
+        <h2>현재 카운트 : {count}</h2>
+      </div>
+      <div>
+        <button className="counter-button" onClick={() => setCount(count - 1)}>-1</button>
+        <button className="counter-button" onClick={() => setCount(count - 10)}>-10</button>
+        <button className="counter-button" onClick={() => setCount(count - 100)}>-100</button>
+        <button className="counter-button" onClick={() => setCount(count + 1)}>+1</button>
+        <button className="counter-button" onClick={() => setCount(count + 10)}>+10</button>
+        <button className="counter-button" onClick={() => setCount(count + 100)}>+100</button>
+      </div>
+    </>
+  )
+}
+
+export default App
