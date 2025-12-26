2d86bb409ea24e7b7c54c412ef7be39154509886
omin <pym000526@gmail.com>
2025-12-26 16:51:00 +0900

    chore:edit project1/App.jsx

--- project1-counter/src/App.jsx
+++ project1-counter/src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleCount = (value) => {
+    setCount(count + value);
+  }
+
   return (
     <>
       <h1>Simple Counter</h1>
@@ -13,12 +17,12 @@ function App() {
         <h2>현재 카운트 : {count}</h2>
       </div>
       <div>
-        <button className="counter-button" onClick={() => setCount(count - 1)}>-1</button>
-        <button className="counter-button" onClick={() => setCount(count - 10)}>-10</button>
-        <button className="counter-button" onClick={() => setCount(count - 100)}>-100</button>
-        <button className="counter-button" onClick={() => setCount(count + 1)}>+1</button>
-        <button className="counter-button" onClick={() => setCount(count + 10)}>+10</button>
-        <button className="counter-button" onClick={() => setCount(count + 100)}>+100</button>
+        <button className="counter-button" onClick={() => (handleCount(-1))}>-1</button>
+        <button className="counter-button" onClick={() => (handleCount(-10))}>-10</button>
+        <button className="counter-button" onClick={() => (handleCount(-100))}>-100</button>
+        <button className="counter-button" onClick={() => (handleCount(+1))}>+1</button>
+        <button className="counter-button" onClick={() => (handleCount(+10))}>+10</button>
+        <button className="counter-button" onClick={() => (handleCount(+100))}>+100</button>
       </div>
     </>
   )
